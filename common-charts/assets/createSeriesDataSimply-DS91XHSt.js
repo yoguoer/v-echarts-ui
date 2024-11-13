b16840541647e4d70d11942e4561a71138fcb5ae
yoguoer <1269148583@qq.com>
2024-11-13 11:16:47 +0800

    deploy

---
+++ common-charts/assets/createSeriesDataSimply-DS91XHSt.js
@@ -0,0 +1,1 @@
+import{am as t,a3 as c,bm as m,ab as o}from"./useDesign-jPeGL5oW.js";function S(e,a,n){a=t(a)&&{coordDimensions:a}||c({encodeDefine:e.getEncode()},a);var r=e.getSource(),s=m(r,a).dimensions,i=new o(s,e);return i.initData(r,n),i}export{S as c};
